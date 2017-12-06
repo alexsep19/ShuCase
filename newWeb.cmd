908879c55aea07d2821d74c55913fb772985d945
alexsep@isonews2.com <alexsep@isonews2.com>
2017-12-06 11:30:59 +0300

    1

---
+++ newWeb.cmd
@@ -0,0 +1,4 @@
+set JAVA_HOME=c:\java\jdk64-7
+set M2_HOME=c:\java\mvn311
+set path=%JAVA_HOME%\bin;%M2_HOME%\bin;%path%
+mvn -s c:\java\mvn311\conf\settings.xml archetype:generate -DarchetypeArtifactId=maven-archetype-webapp -DgroupId=shu -DartifactId=web -DinteractiveMode=false
